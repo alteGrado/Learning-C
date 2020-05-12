83e8bae697b8bd14f08c28e89b03402afc50c0c3
alteGrado <60178039+alteGrado@users.noreply.github.com>
2020-05-12 22:52:52 +0300

    Update arguments.c

--- arguments.c
+++ arguments.c
@@ -1,3 +1,10 @@
+//In C,access to command line arguments can be obtained through the main()
+//function by specifying two additional arguments for it: an integer and a 
+//pointer to an array of strings. An integer specifies the number of command
+//line arguments, and an array of strings holds all these arguments.
+
+/* gcc -Wall -g arguments.c -o arguments */
+
 #include <stdio.h>
 
 int main(int arg_count, char *arg_list[]) 
